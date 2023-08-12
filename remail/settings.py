f853461b19b4b01507f41424da394cc509cf7eec
Pinethanku <pinethanku21@gmail.com>
2023-08-12 15:27:09 +0900

    [sol] 0812 mailbox 추가

--- remail/settings.py
+++ remail/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'home',
     'login',
     'signup',
+    'remailbox',
 ]
 
 MIDDLEWARE = [
--- remail/urls.py
+++ remail/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', RedirectView.as_view(url="home/", permanent=True)),
     path('home/', include('login.urls')),
     path('home/', include('signup.urls')),
+    path('remailbox/', include('remailbox.urls')),
 ]
