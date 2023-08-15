11208d48905f3d65e8b4227b8ca33edc4a1540f5
Pinethanku <pinethanku21@gmail.com>
2023-08-15 19:16:17 +0900

    [sol] 0815 유우허김 + user 데이터 불러오기

--- remailbox/views.py
+++ remailbox/views.py
@@ -1,26 +1,37 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
+from user.models import User
+from home.views import hello
 
 # Create your views here.
 
 def mailbox(request):
-    if request.method == 'POST':
-        return render(request, 'remailbox/mail_read.html', {'title': request.POST['selected']})
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            return render(request, 'remailbox/mail_read.html', {'title': request.POST['selected']})
     
-    maillist = [ #수신자 계정으로 받은 메일 리스트를 입력합니다
-                  {'no':1, 'sender': '보낸사람1', 'time' : '2023.08.13\t12:34', 'title':'[중요]제목1'}, 
-                  {'no':2, 'sender': '보낸사람2', 'time' : '2023.08.13\t56:78', 'title':'[중요]제목2'},
-                  {'no':3, 'sender': '보낸사람3', 'time' : '2023.08.13\t90:12', 'title':'[중요]제목3'},
-                  {'no':4, 'sender': '보낸사람4', 'time' : '2023.08.13\t34:56', 'title':'[중요]제목4'},
-                  {'no':5, 'sender': '보낸사람5', 'time' : '2023.08.13\t78:90', 'title':'[중요]제목5'}
-    ]
+        maillist = [ #수신자 계정으로 받은 메일 리스트를 입력합니다
+                    {'no':1, 'sender': '보낸사람1', 'time' : '2023.08.13\t12:34', 'title':'[중요]제목1'}, 
+                    {'no':2, 'sender': '보낸사람2', 'time' : '2023.08.13\t56:78', 'title':'[중요]제목2'},
+                    {'no':3, 'sender': '보낸사람3', 'time' : '2023.08.13\t90:12', 'title':'[중요]제목3'},
+                    {'no':4, 'sender': '보낸사람4', 'time' : '2023.08.13\t34:56', 'title':'[중요]제목4'},
+                    {'no':5, 'sender': '보낸사람5', 'time' : '2023.08.13\t78:90', 'title':'[중요]제목5'}
+        ]
+        
+        return render(request, 'remailbox/mailbox.html', {'maillist': maillist})
+        
+    else:
+        return redirect('home')
     
-    return render(request, 'remailbox/mailbox.html', {'maillist': maillist})
 
 def mail_write(request):
+    user_content = User.objects.all()
+    content = {'user_content' : user_content}
     
-    return render(request, 'remailbox/mail_write.html')
+    print(content)
+    
+    return render(request, 'remailbox/mail_write.html', content)
 
 def mail_read(request):
     if request.method == 'POST':
--- user/views.py
+++ user/views.py
@@ -1,73 +1,55 @@
 from django.shortcuts import render, redirect
-from django.db import transaction
-from .models import User
-from argon2 import PasswordHasher
-from .forms import RegisterForm, LoginForm
-from remailbox.views import mailbox
+from django.contrib import auth
+#from django.contrib.auth.models import User  
+from django.contrib import messages
+from django.db.utils import IntegrityError
+from .models import User  
 
-# Create your views here.
+
 def register(request):
-    register_form = RegisterForm()
-    context = {'forms' : register_form}
-
     if request.method == 'GET':
-        return render(request, 'user/register.html', context)
+        return render(request, 'user/register.html')
     
-    elif request.method =='POST':
-        # user_email = request.POST.get('email','')
-        # user_name = request.POST.get('name','')
-        # user_pw = request.POST.get('pw','')
-        # user_pw_confirm = request.POST.get('pw-confirm','')
-
-        # if (user_email or user_name or user_pw or user_pw_confirm) == '':
-        #     return redirect('/user/register')
-        # elif user_pw !=user_pw_confirm:
-        #     return redirect('user/register')
-        # else:
-        #     user=User(
-        #         user_email = user_email,
-        #         user_name = user_name,
-        #         user_pw = user_pw
-        #     )
-        #     user.save()
-        # return redirect('/')
-        register_form = RegisterForm(request.POST)
-        if register_form.is_valid():
-            user=User(
-                user_email = register_form.user_email,
-                user_name = register_form.user_name,
-                user_pw = register_form.user_pw
-            )
-            user.save()
-            return redirect('/')
+    elif request.method == "POST":
+        if 4 > len(request.POST["username"]) or len(request.POST["username"]) > 20:
+            return render(request, "user/register.html", {"data": "id_too"})
+        if request.POST["password1"] == request.POST["password2"]:
+            if 5 > len(request.POST["password1"]) or len(request.POST["password1"]) > 15:
+                return render(request, "user/register.html", {"data": "password_too"})
+            try:
+                user = User.objects.create_user(username=request.POST["username"],
+                                                password=request.POST["password1"],
+                                                email=request.POST["email"])
+            except IntegrityError:
+                return render(request, "user/register.html", {"data": "id_overlap"})
+            auth.login(request, user)
+            messages.info(request, "회원가입이 완료되었습니다!")
+            return redirect("/home/")
         else:
-            context['forms'] = register_form
-            if register_form.errors:
-                for value in register_form.errors.values():
-                    context['error'] = value
-        return render(request, 'user/register.html',context)
+            return render(request, "user/register.html", {"data": "check_not_same"})
+        
 
-def login(request):
-    loginform = LoginForm()
-    context = {'forms' : loginform}
+# def login(request):
+#     loginform = LoginForm()
+#     context = {'forms' : loginform}
 
-    if request.method == 'GET':
-        return render(request, 'user/login.html', context)
+#     if request.method == 'GET':
+#         return render(request, 'user/login.html', context)
     
-    elif request.method == 'POST':
-        loginform = LoginForm(request.POST)
+#     elif request.method == 'POST':
+#         loginform = LoginForm(request.POST)
 
-        if loginform.is_valid():
-            request.session['login_session'] = loginform.login_session
-            request.session.set_expiry(0)
-            return redirect('mailbox')
-        else:
-            context['forms'] = loginform
-            if loginform.errors:
-                for value in loginform.errors.values():
-                    context['error'] = value
-        return render(request, 'uesr/login.html', context)
-#수정  
-def logout(request):
-    request.session.flush()
-    return redirect('/')
+#         if loginform.is_valid(): #로그인 성공 시
+#             request.session['login_session'] = loginform.login_session
+#             request.session.set_expiry(0)
+#             return redirect('mailbox') #mailbox로 이동
+#         else:
+#             context['forms'] = loginform
+#             if loginform.errors:
+#                 for value in loginform.errors.values():
+#                     context['error'] = value
+#         return render(request, 'uesr/login.html', context)
+# #수정  
+# def logout(request):
+#     request.session.flush()
+#     return redirect('/')
